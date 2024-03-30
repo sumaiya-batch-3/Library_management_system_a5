24d288e5394f5cd954ce4d34fa08af22ab0ea30e
Sumaiya Sharmin <ssumaiya@live.com>
2024-03-30 10:35:23 -0700

    database added

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://library-management-system-a5-1.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
